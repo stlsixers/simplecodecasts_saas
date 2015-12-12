b577fca20b0cca10e9079bb72b425b8da6d6f923
Kevin C <kevin.chan@uap.asia>
2015-12-12 11:57:56 +0800

    added users show page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,12 +6,13 @@ class ProfilesController < ApplicationController
 	end
 
 	def create
-		@user = User.find (params[:user_id])
+		@user = User.find(params[:user_id])
 		@profile = @user.build_profile(profile_params)
 		if @profile.save
-			redirec_to user_path(params[:user.id])
+			flash[:success] = "Profile Updated!"
+			redirect_to user_path(params[:user_id])
 		else
-
+			render action: :new
 		end
 	end
 
