92f3161077ab7ad8afe64be4b1f2541cf162c92e
Kevin C <kevin.chan@uap.asia>
2015-12-12 11:18:35 +0800

    added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,19 @@ class ProfilesController < ApplicationController
 		@user = User.find(params[:user_id])
 		@profile = @user.build_profile
 	end
+
+	def create
+		@user = User.find (params[:user_id])
+		@profile = @user.build_profile(profile_params)
+		if @profile.save
+			redirec_to user_path(params[:user.id])
+		else
+
+		end
+	end
+
+	private
+		def profile_params
+			params.require(:profile).permit(:first_name,:last_name,:phone_number,:contact_email,:description)
+		end
 end
